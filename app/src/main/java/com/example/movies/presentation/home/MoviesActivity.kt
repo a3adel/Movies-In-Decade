4a7cc1d99de200b5e8cd5d228bfae746e3737a2e
Adel <a3adel@hotmail.com>
2021-03-26 17:35:50 +0200

    building the presentation layer for the retriving all movies module

--- app/src/main/java/com/example/movies/presentation/home/MoviesActivity.kt
+++ app/src/main/java/com/example/movies/presentation/home/MoviesActivity.kt
@@ -1,15 +1,43 @@
 package com.example.movies.presentation.home
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.movies.R
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.movies.databinding.ActivityMoviesBinding
+import com.example.movies.domain.models.Movie
+import com.example.movies.presentation.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MoviesActivity : AppCompatActivity() {
+class MoviesActivity : BaseActivity() {
+    private lateinit var binder: ActivityMoviesBinding
+    private val moviesViewModel: MoviesViewModel by viewModels()
+    private val adapter = MoviesAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
+        binder = ActivityMoviesBinding.inflate(layoutInflater)
+        setContentView(binder.root)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        moviesViewModel.getMovies()
+    }
+
+    override fun initViews() {
+        val layoutManager = LinearLayoutManager(this)
+        binder.moviesRecyclerView.layoutManager = layoutManager
+        binder.moviesRecyclerView.adapter = adapter
+    }
+
+    override fun observeViewModels() {
+        moviesViewModel.moviesListLiveData.observe(this, ::updateMoviesList)
+        moviesViewModel.showProgressBarLiveData.observe(this,::observeShowProgressDialog)
+        moviesViewModel.hideProgressBarLiveData.observe(this,::observeHideProgressDialog)
+        moviesViewModel.toastLiveData.observe(this,::handleToast)
+    }
+
+    private fun updateMoviesList(list: List<Movie>) {
+        Log . d ("VIEWMODELMOVIES","ACTIVITY ${list.size}")
+
+        adapter.submitList(list)
     }
 
 
